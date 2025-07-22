f05e46e7bee0b929fe35e8e3e832551f89856e12
1thallo <ithalloflu@gmail.com>
2025-07-21 23:54:48 -0300

    📆21/07 - Apresentação da PEP 8, Isort, Black formatter e Flake8

---
+++ 5.Trabalhando com pacotes/Cursos/4.Pacotes,convenções e boas práticas/pep_8.py
@@ -0,0 +1,12 @@
+def somar(argumento_1, argumento_2):    #snake_case
+    pass
+
+class ContaBancaria:        #CamelCase
+    pass
+
+# uso do flake8
+# pip install flake8
+# flake8 arquivo.py
+
+# Black - ferramenta de formatação
+# Isort
